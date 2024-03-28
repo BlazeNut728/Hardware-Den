d10f29d40553abe54702a015135c6d91e4c57a3a
Aryan Sharma <107105468+BlazeNut728@users.noreply.github.com>
2024-03-28 16:39:26 +0530

    Added Loading Bar + PageName

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
 
     filter_data();
+    console.log("Current page name:", pageName);
 
     function filter_data()
     {
-        $('.laptop-item').html('');
+        $('.laptop-item').html('<div style="text-align:center;"><img src="https://media.discordapp.net/attachments/1196163965110202440/1222860404804554762/Loading-bar.gif?ex=6617c080&is=66054b80&hm=43b803345d4c746895d2a04e08921d207ad51c0510579764f1777f9da13fbaf2&=" alt="Loading..." /></div>');
         var action = 'fetch_data';
         var category = get_filter('category');
         var manufacturer = get_filter('manufacturer');
         var cpu_brand = get_filter('cpu_brand');
         var ram_size = get_filter('ram_size');
         var storage_size = get_filter('storage_size');
+        var page_name = pageName;
+
         $.ajax({
-            url:"fetch_data.php",
+            url:"fetch_data.php?page_name=" + page_name,
             method:"POST",
-            data:{action:action, category:category, manufacturer:manufacturer, cpu_brand:cpu_brand, ram_size:ram_size, storage_size:storage_size},
+            data:{action:action, category:category, manufacturer:manufacturer, cpu_brand:cpu_brand, ram_size:ram_size, storage_size:storage_size, page_name: page_name},
             success:function(data){
-                $('.laptop-item').html(data);
+                $('.item').html(data);
             }
         });
     }
