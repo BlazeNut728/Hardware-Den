8d3d4a20fafc8b66d95a6ed5a1763be99bf5c2c3
Aryan Sharma <107105468+BlazeNut728@users.noreply.github.com>
2024-03-30 23:30:13 +0530

    Details.php | Laptop & PC Calibrated

--- src/php/details.php
+++ src/php/details.php
@@ -37,6 +37,11 @@
             $sql = "SELECT * FROM pc WHERE pc_id = '$id'";
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_assoc($result);
+
+            $id= $row['pc_id'];
+            
+            $imageLinks = explode("\n", $row["carousel_images"]);
+            $descText = explode("\n", $row["about"]);
         }
     ?>
 
@@ -67,47 +72,82 @@
         </div>
 
         <div class="detail-grid-item">
-            <a class="detail-title"><?php echo $row["release_year"].' '. $row["manufacturer"].' '.$row["model_name"] ?> </a> <input type="submit" value="Add To Cart" class="item-submit"> <br> 
-                
-            <div class="divider"></div><br>
-            
-            <a class="detail-price">₹ <?php echo $row["price"] ?> </a><br><br>
+        <?php
+        if($id >= 1 && $id <= 999999)
+        {
+            echo '<a class="detail-title">' . $row["release_year"] . ' ' . $row["manufacturer"] . ' ' . $row["model_name"] . '</a> <input type="submit" value="Add To Cart" class="item-submit"> <br>';
+            echo '<div class="divider"></div><br>';
+            echo '<a class="detail-price">₹ ' . $row["price"] . '</a><br><br>';
+            echo '<a class="detail-spec-heading">Specifications:</a>';
+            echo '<ul class="detail-spec-desc">';
+            echo '<li>Brand: ' . $row["manufacturer"] . '</li>';
+            echo '<li>Model Name: ' . $row["model_name"] . '</li>';
+            echo '<li>Category: ' . $row["category"] . '</li>';
+            echo '<li>Operating System: ' . $row["os"] . '</li>';
+            echo '<li>Screen: ' . $row["screen_reso"] . ' | ' . $row["screen_size"] . '" Inches | ' . $row["panel_type"] . '</li>';
+            echo '<li>Processor: ' . $row["cpu_brand"] . ' ' . $row["cpu_series"] . ' ' . $row["cpu_name"] . ' | Base Clock: ' . $row["cpu_speed"] . ' Ghz</li>';
+            echo '<li>Graphics Card: ' . $row["gpu_type"] . ' | ' . $row["gpu_brand"] . ' ' . $row["gpu_series"] . ' | VRAM: ' . $row["vram"] . ' MB</li>';
+            echo '<li>RAM: ' . $row["ram_size"] . 'GB DDR5 | ' . $row["ram_speed"] . ' MHz</li>';
+            echo '<li>Storage: ' . $row["storage_type"] . ' | ' . $row["storage_size"] . ' GB</li>';
+            echo '<li>Weight: ' . $row["weight"] . ' KG</li>';
+            echo '</ul>';
+            echo '<div class="icon-container">';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-rotate-left"></i>';
+            echo '<br>7 Day <br> Replacement';
+            echo '</div>';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-truck-ramp-box"></i>';
+            echo '<br>Free <br> Delivery';
+            echo '</div>';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-calendar-check"></i>';
+            echo '<br>Warranty <br> Policy';
+            echo '</div>';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-award"></i>';
+            echo '<br>Top <br> Brand';
+            echo '</div>';
+            echo '</div>';
+        }
 
-            <a class="detail-spec-heading">Specifications:</a>
+        else if($id >= 1000000 && $id <= 1999999)
+        {
+            echo '<a class="detail-title">' . $row["manufacturer"] . ' ' . $row["model_name"] . '</a> <input type="submit" value="Add To Cart" class="item-submit"> <br>';
+            echo '<div class="divider"></div><br>';
+            echo '<a class="detail-price">₹ ' . $row["price"] . '</a><br><br>';
+            echo '<a class="detail-spec-heading">Specifications:</a>';
+            echo '<ul class="detail-spec-desc">';
+            echo '<li>Brand: ' . $row["manufacturer"] . '</li>';
+            echo '<li>Model Name: ' . $row["model_name"] . '</li>';
+            echo '<li>Category: ' . $row["category"] . '</li>';
+            echo '<li>Processor: ' . $row["cpu_brand"] . ' ' . $row["cpu_series"] . ' ' . $row["cpu_name"] . ' | Base Clock: ' . $row["cpu_speed"] . ' Ghz</li>';
+            echo '<li>Graphics Card: ' . $row["gpu_type"] . ' | ' . $row["gpu_brand"] . ' ' . $row["gpu_series"] . ' | VRAM: ' . $row["vram"] . ' MB</li>';
+            echo '<li>RAM: ' . $row["ram_size"] . 'GB DDR5 | ' . $row["ram_speed"] . ' MHz</li>';
+            echo '<li>Storage: ' . $row["storage_type"] . ' | ' . $row["storage_size"] . ' GB</li>';
+            echo '<li>Power Supply: ' . $row["power_supply"] . '' . $row["power_supply_detail"] . '</li>';
+            echo '</ul>';
+            echo '<div class="icon-container">';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-rotate-left"></i>';
+            echo '<br>7 Day <br> Replacement';
+            echo '</div>';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-truck-ramp-box"></i>';
+            echo '<br>Free <br> Delivery';
+            echo '</div>';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-calendar-check"></i>';
+            echo '<br>Warranty <br> Policy';
+            echo '</div>';
+            echo '<div class="icon-box">';
+            echo '<i class="fa-solid fa-award"></i>';
+            echo '<br>Top <br> Brand';
+            echo '</div>';
+            echo '</div>';
+        }
+        ?>
 
-            <ul class = "detail-spec-desc" >
-                <li>Brand: <?php echo $row["manufacturer"] ?> </li>
-                <li>Model Name: <?php echo $row["model_name"] ?> </li>
-                <li>Category: <?php echo $row["category"] ?> </li>
-                <li>Operating System: <?php echo $row["os"] ?> </li>
-                <li>Screen: <?php echo $row["screen_reso"].' | '. $row["screen_size"] . '" Inches | ' . $row["panel_type"] ?></li>
-                <li>Processor: <?php echo $row["cpu_brand"].' '. $row["cpu_series"] . ' ' . $row["cpu_name"] . ' | Base Clock: ' . $row["cpu_speed"] ?> Ghz </li>
-                <li>Graphics Card: <?php echo $row["gpu_type"].' | '. $row["gpu_brand"] . ' ' . $row["gpu_series"] . ' | VRAM: ' . $row["vram"] ?> MB </li>
-                <li>RAM: <?php echo $row["ram_size"] .'GB DDR5 | '. $row["ram_speed"] ?> MHz</li>
-                <li>Storage: <?php echo $row["storage_type"] .' | '. $row["storage_size"] ?> GB</li>
-                <li>Weight: <?php echo $row["weight"] ?> KG</li>
-            </ul>
-
-            <div class = "icon-container">
-
-                <div class = "icon-box">
-                <i class="fa-solid fa-rotate-left"></i>
-                <br>7 Day <br> Replacement
-                </div>
-                <div class = "icon-box">
-                <i class="fa-solid fa-truck-ramp-box"></i>
-                <br>Free <br> Delivery
-                </div>
-                <div class = "icon-box">
-                <i class="fa-solid fa-calendar-check"></i>
-                <br>Warranty <br> Policy
-                </div>
-                <div class = "icon-box">
-                <i class="fa-solid fa-award"></i>
-                <br>Top <br> Brand
-                </div>
-
-            </div>
 
         </div>
     </div>
@@ -116,22 +156,39 @@
 
     <div class="desc-grid-container">
         
-        <div class="desc-grid-item">
-            <ul class = "detail-spec-desc" style = "margin-left: 9.1vw; margin-right: 9.1vw;" >
-                <?php foreach ($descText as $index => $descText) {
-                    echo '<li>'. $descText . '</li> <br>';
-                } ?>
-            </ul>
-        </div>
+        <?php
+            if($id >= 1 && $id <= 999999)
+            {
+                echo '<div class="desc-grid-item">';
+                echo '<ul class="detail-spec-desc" style="margin-left: 9.1vw; margin-right: 9.1vw;">';
+                foreach ($descText as $index => $text) {
+                    echo '<li>' . $text . '</li><br>';
+                }
+                echo '</ul>';
+                echo '</div>';
 
-        <div class="divider" style = "width: 100%;"></div><br>
+                echo '<div class="divider" style="width: 100%;"></div><br>';
 
-        <div class="desc-grid-item" style = "justify-items: center;">
-            <?php foreach ($descLinks as $index => $descLinks) {
-                echo '<img src="' . $descLinks . '" alt="" style = "max-width: 78.125vw; max-height: 77.47vh; margin-left: 9.1vw; margin-right: 9.1vw;">';
-            } ?>
+                echo '<div class="desc-grid-item" style="justify-items: center;">';
+                foreach ($descLinks as $index => $link) {
+                    echo '<img src="' . $link . '" alt="" style="max-width: 78.125vw; max-height: 77.47vh; display: block;margin-left: auto;margin-right: auto;">';
+                }
+                echo '</div>';
+            }
+            else if($id >= 100000 && $id <= 1999999)
+            {
+                echo '<div class="desc-grid-item">';
+                echo '<ul class="detail-spec-desc" style="margin-left: 9.1vw; margin-right: 9.1vw;">';
+                foreach ($descText as $index => $text) {
+                    echo '<li>' . $text . '</li><br>';
+                }
+                echo '</ul>';
+                echo '</div>';
 
-        </div>
+                echo '<div class="divider" style="width: 100%;"></div><br>';
+            }
+        ?>
+
 
     </div>
 
